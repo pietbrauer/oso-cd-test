c6edf57e48fb35f101b408be82f501252ebbb876
Piet Brauer <piet.brauer@mhp.com>
2020-03-03 06:09:22 +0100

    Updates

---
+++ precache-manifest.e2b4e748ee8ef43f8ee07b88248484bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8d14bbbea3f045885247678e9726b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "892e24d8df5d6b891852",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "291e755bcab7e2efe4cd",
+    "url": "/static/js/2.8437322e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8437322e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892e24d8df5d6b891852",
+    "url": "/static/js/main.d261b063.chunk.js"
+  },
+  {
+    "revision": "5eb7b7d8c12f9a9ac022",
+    "url": "/static/js/runtime-main.02e819a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
